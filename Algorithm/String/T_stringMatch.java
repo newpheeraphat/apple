769da322ba750cae98c691bd22b8c2011c6003d0
newpheeraphat <77052746+newpheeraphat@users.noreply.github.com>
2021-08-07 16:46:49 +0700

    Create T_stringMatch.java

---
+++ Algorithm/String/T_stringMatch.java
@@ -0,0 +1,12 @@
+public int stringMatch(String a, String b){
+      int len = Math.min(a.length(), b.length());
+      int _count = 0;
+      for (int i = 0 ; i < len-1 ; i++){
+         String subA = a.substring(i, i + 2);
+         String subB = b.substring(i, i + 2);
+         if (subA.equals(subB)){
+            _count++;
+         }
+      }
+      return _count ;
+   }
