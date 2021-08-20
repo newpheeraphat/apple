c8d5d1d59b053bc99f145f2659b28d5a3e684438
newpheeraphat <77052746+newpheeraphat@users.noreply.github.com>
2021-08-21 00:12:23 +0700

    Create T_capitalizeFirst.java

---
+++ Algorithm/String/T_capitalizeFirst.java
@@ -0,0 +1,4 @@
+if (num == 1){
+         return "Hello " + Character.toUpperCase(name.charAt(0)) + name.substring(1);
+      }
+      return "Bye " + Character.toUpperCase(name.charAt(0)) + name.substring(1);
